e8d5da60ed7452fa2ae1bda1dfec63b781637454
xiaodaxia-2008 <xiaozisheng2008@qq.com>
2019-03-26 20:27:51 +0800

    initial version

---
+++ output/StateServer.d
@@ -0,0 +1,131 @@
+C:\Users\xiaoz\Documents\MotominiDriver\output\StateServer.a:  \
+ C:\Users\xiaoz\AppData\Local\MotoPlusIDE_YRCmicro\ 0\StateServer.c \
+  C:\Users\xiaoz\AppData\Local\MotoPlusIDE_YRCmicro\ 0\/MotoROS.h \
+  C:\Program\ Files\ (x86)\Yaskawa\MotoPlusIDE_YRCmicro\mpbuilder\inc/motoPlus.h \
+  C:\Program\ Files\ (x86)\Yaskawa\MotoPlusIDE_YRCmicro\mpbuilder\inc/mpParams.h \
+  c:\program\ files\ (x86)\yaskawa\motopluside_yrcmicro\mpbuilder\gnu\4.3.3-vxworks-6.9\x86-win32\bin\../../lib/gcc/i586-wrs-vxworks/4.3.3/include/stddef.h \
+  C:\Program\ Files\ (x86)\Yaskawa\MotoPlusIDE_YRCmicro\mpbuilder\inc/limits.h \
+  C:\Program\ Files\ (x86)\Yaskawa\MotoPlusIDE_YRCmicro\mpbuilder\inc/stdio.h \
+  C:\Program\ Files\ (x86)\Yaskawa\MotoPlusIDE_YRCmicro\mpbuilder\inc/_mingw.h \
+  c:\program\ files\ (x86)\yaskawa\motopluside_yrcmicro\mpbuilder\gnu\4.3.3-vxworks-6.9\x86-win32\bin\../../lib/gcc/i586-wrs-vxworks/4.3.3/include/stdarg.h \
+  C:\Program\ Files\ (x86)\Yaskawa\MotoPlusIDE_YRCmicro\mpbuilder\inc/stdlib.h \
+  C:\Program\ Files\ (x86)\Yaskawa\MotoPlusIDE_YRCmicro\mpbuilder\inc/string.h \
+  C:\Program\ Files\ (x86)\Yaskawa\MotoPlusIDE_YRCmicro\mpbuilder\inc/math.h \
+  C:\Program\ Files\ (x86)\Yaskawa\MotoPlusIDE_YRCmicro\mpbuilder\inc/ctype.h \
+  C:\Program\ Files\ (x86)\Yaskawa\MotoPlusIDE_YRCmicro\mpbuilder\inc/errno.h \
+  C:\Program\ Files\ (x86)\Yaskawa\MotoPlusIDE_YRCmicro\mpbuilder\inc/time.h \
+  C:\Program\ Files\ (x86)\Yaskawa\MotoPlusIDE_YRCmicro\mpbuilder\inc/mpSpecDef.h \
+  C:\Program\ Files\ (x86)\Yaskawa\MotoPlusIDE_YRCmicro\mpbuilder\inc/mpSysCtrl.h \
+  C:\Program\ Files\ (x86)\Yaskawa\MotoPlusIDE_YRCmicro\mpbuilder\inc/mpTaskCtrl.h \
+  C:\Program\ Files\ (x86)\Yaskawa\MotoPlusIDE_YRCmicro\mpbuilder\inc/mpFileCtrl.h \
+  C:\Program\ Files\ (x86)\Yaskawa\MotoPlusIDE_YRCmicro\mpbuilder\inc/mpMemPart.h \
+  C:\Program\ Files\ (x86)\Yaskawa\MotoPlusIDE_YRCmicro\mpbuilder\inc/mpKmcEvnt.h \
+  C:\Program\ Files\ (x86)\Yaskawa\MotoPlusIDE_YRCmicro\mpbuilder\inc/mpEtherNet.h \
+  C:\Program\ Files\ (x86)\Yaskawa\MotoPlusIDE_YRCmicro\mpbuilder\inc/mpExtraRcs.h \
+  C:\Program\ Files\ (x86)\Yaskawa\MotoPlusIDE_YRCmicro\mpbuilder\inc/mpSensCtrl.h \
+  C:\Program\ Files\ (x86)\Yaskawa\MotoPlusIDE_YRCmicro\mpbuilder\inc/mpSrvoCtrl.h \
+  C:\Program\ Files\ (x86)\Yaskawa\MotoPlusIDE_YRCmicro\mpbuilder\inc/mpSrvoFcs.h \
+  C:\Program\ Files\ (x86)\Yaskawa\MotoPlusIDE_YRCmicro\mpbuilder\inc/mpCioDef.h \
+  C:\Program\ Files\ (x86)\Yaskawa\MotoPlusIDE_YRCmicro\mpbuilder\inc/mpStopWatch.h \
+  C:\Program\ Files\ (x86)\Yaskawa\MotoPlusIDE_YRCmicro\mpbuilder\inc/mpLegApi00.h \
+  C:\Program\ Files\ (x86)\Yaskawa\MotoPlusIDE_YRCmicro\mpbuilder\inc/mpExFileCtrl.h \
+  C:\Program\ Files\ (x86)\Yaskawa\MotoPlusIDE_YRCmicro\mpbuilder\inc/mpWatchDog.h \
+  C:\Program\ Files\ (x86)\Yaskawa\MotoPlusIDE_YRCmicro\mpbuilder\inc/mpKinema.h \
+  C:\Program\ Files\ (x86)\Yaskawa\MotoPlusIDE_YRCmicro\mpbuilder\inc/mpMotCtrl.h \
+  C:\Program\ Files\ (x86)\Yaskawa\MotoPlusIDE_YRCmicro\mpbuilder\inc/mpDevTool.h \
+  C:\Program\ Files\ (x86)\Yaskawa\MotoPlusIDE_YRCmicro\mpbuilder\inc/mpUsrCdef.h \
+  C:\Users\xiaoz\AppData\Local\MotoPlusIDE_YRCmicro\ 0\/ParameterExtraction.h \
+  C:\Users\xiaoz\AppData\Local\MotoPlusIDE_YRCmicro\ 0\/ParameterTypes.h \
+  C:\Users\xiaoz\AppData\Local\MotoPlusIDE_YRCmicro\ 0\/CtrlGroup.h \
+  C:\Users\xiaoz\AppData\Local\MotoPlusIDE_YRCmicro\ 0\/SimpleMessage.h \
+  C:\Users\xiaoz\AppData\Local\MotoPlusIDE_YRCmicro\ 0\/Controller.h \
+  C:\Users\xiaoz\AppData\Local\MotoPlusIDE_YRCmicro\ 0\/IoServer.h \
+  C:\Users\xiaoz\AppData\Local\MotoPlusIDE_YRCmicro\ 0\/MotionServer.h \
+  C:\Users\xiaoz\AppData\Local\MotoPlusIDE_YRCmicro\ 0\/StateServer.h \
+  C:\Users\xiaoz\AppData\Local\MotoPlusIDE_YRCmicro\ 0\/RosSetupValidation.h
+
+C:\Users\xiaoz\AppData\Local\MotoPlusIDE_YRCmicro\ 0\/MotoROS.h:
+
+C:\Program\ Files\ (x86)\Yaskawa\MotoPlusIDE_YRCmicro\mpbuilder\inc/motoPlus.h:
+
+C:\Program\ Files\ (x86)\Yaskawa\MotoPlusIDE_YRCmicro\mpbuilder\inc/mpParams.h:
+
+c:\program\ files\ (x86)\yaskawa\motopluside_yrcmicro\mpbuilder\gnu\4.3.3-vxworks-6.9\x86-win32\bin\../../lib/gcc/i586-wrs-vxworks/4.3.3/include/stddef.h:
+
+C:\Program\ Files\ (x86)\Yaskawa\MotoPlusIDE_YRCmicro\mpbuilder\inc/limits.h:
+
+C:\Program\ Files\ (x86)\Yaskawa\MotoPlusIDE_YRCmicro\mpbuilder\inc/stdio.h:
+
+C:\Program\ Files\ (x86)\Yaskawa\MotoPlusIDE_YRCmicro\mpbuilder\inc/_mingw.h:
+
+c:\program\ files\ (x86)\yaskawa\motopluside_yrcmicro\mpbuilder\gnu\4.3.3-vxworks-6.9\x86-win32\bin\../../lib/gcc/i586-wrs-vxworks/4.3.3/include/stdarg.h:
+
+C:\Program\ Files\ (x86)\Yaskawa\MotoPlusIDE_YRCmicro\mpbuilder\inc/stdlib.h:
+
+C:\Program\ Files\ (x86)\Yaskawa\MotoPlusIDE_YRCmicro\mpbuilder\inc/string.h:
+
+C:\Program\ Files\ (x86)\Yaskawa\MotoPlusIDE_YRCmicro\mpbuilder\inc/math.h:
+
+C:\Program\ Files\ (x86)\Yaskawa\MotoPlusIDE_YRCmicro\mpbuilder\inc/ctype.h:
+
+C:\Program\ Files\ (x86)\Yaskawa\MotoPlusIDE_YRCmicro\mpbuilder\inc/errno.h:
+
+C:\Program\ Files\ (x86)\Yaskawa\MotoPlusIDE_YRCmicro\mpbuilder\inc/time.h:
+
+C:\Program\ Files\ (x86)\Yaskawa\MotoPlusIDE_YRCmicro\mpbuilder\inc/mpSpecDef.h:
+
+C:\Program\ Files\ (x86)\Yaskawa\MotoPlusIDE_YRCmicro\mpbuilder\inc/mpSysCtrl.h:
+
+C:\Program\ Files\ (x86)\Yaskawa\MotoPlusIDE_YRCmicro\mpbuilder\inc/mpTaskCtrl.h:
+
+C:\Program\ Files\ (x86)\Yaskawa\MotoPlusIDE_YRCmicro\mpbuilder\inc/mpFileCtrl.h:
+
+C:\Program\ Files\ (x86)\Yaskawa\MotoPlusIDE_YRCmicro\mpbuilder\inc/mpMemPart.h:
+
+C:\Program\ Files\ (x86)\Yaskawa\MotoPlusIDE_YRCmicro\mpbuilder\inc/mpKmcEvnt.h:
+
+C:\Program\ Files\ (x86)\Yaskawa\MotoPlusIDE_YRCmicro\mpbuilder\inc/mpEtherNet.h:
+
+C:\Program\ Files\ (x86)\Yaskawa\MotoPlusIDE_YRCmicro\mpbuilder\inc/mpExtraRcs.h:
+
+C:\Program\ Files\ (x86)\Yaskawa\MotoPlusIDE_YRCmicro\mpbuilder\inc/mpSensCtrl.h:
+
+C:\Program\ Files\ (x86)\Yaskawa\MotoPlusIDE_YRCmicro\mpbuilder\inc/mpSrvoCtrl.h:
+
+C:\Program\ Files\ (x86)\Yaskawa\MotoPlusIDE_YRCmicro\mpbuilder\inc/mpSrvoFcs.h:
+
+C:\Program\ Files\ (x86)\Yaskawa\MotoPlusIDE_YRCmicro\mpbuilder\inc/mpCioDef.h:
+
+C:\Program\ Files\ (x86)\Yaskawa\MotoPlusIDE_YRCmicro\mpbuilder\inc/mpStopWatch.h:
+
+C:\Program\ Files\ (x86)\Yaskawa\MotoPlusIDE_YRCmicro\mpbuilder\inc/mpLegApi00.h:
+
+C:\Program\ Files\ (x86)\Yaskawa\MotoPlusIDE_YRCmicro\mpbuilder\inc/mpExFileCtrl.h:
+
+C:\Program\ Files\ (x86)\Yaskawa\MotoPlusIDE_YRCmicro\mpbuilder\inc/mpWatchDog.h:
+
+C:\Program\ Files\ (x86)\Yaskawa\MotoPlusIDE_YRCmicro\mpbuilder\inc/mpKinema.h:
+
+C:\Program\ Files\ (x86)\Yaskawa\MotoPlusIDE_YRCmicro\mpbuilder\inc/mpMotCtrl.h:
+
+C:\Program\ Files\ (x86)\Yaskawa\MotoPlusIDE_YRCmicro\mpbuilder\inc/mpDevTool.h:
+
+C:\Program\ Files\ (x86)\Yaskawa\MotoPlusIDE_YRCmicro\mpbuilder\inc/mpUsrCdef.h:
+
+C:\Users\xiaoz\AppData\Local\MotoPlusIDE_YRCmicro\ 0\/ParameterExtraction.h:
+
+C:\Users\xiaoz\AppData\Local\MotoPlusIDE_YRCmicro\ 0\/ParameterTypes.h:
+
+C:\Users\xiaoz\AppData\Local\MotoPlusIDE_YRCmicro\ 0\/CtrlGroup.h:
+
+C:\Users\xiaoz\AppData\Local\MotoPlusIDE_YRCmicro\ 0\/SimpleMessage.h:
+
+C:\Users\xiaoz\AppData\Local\MotoPlusIDE_YRCmicro\ 0\/Controller.h:
+
+C:\Users\xiaoz\AppData\Local\MotoPlusIDE_YRCmicro\ 0\/IoServer.h:
+
+C:\Users\xiaoz\AppData\Local\MotoPlusIDE_YRCmicro\ 0\/MotionServer.h:
+
+C:\Users\xiaoz\AppData\Local\MotoPlusIDE_YRCmicro\ 0\/StateServer.h:
+
+C:\Users\xiaoz\AppData\Local\MotoPlusIDE_YRCmicro\ 0\/RosSetupValidation.h:
